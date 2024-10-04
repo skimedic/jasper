548b9c30e1d9fd36698a4d514af4a1fb52944822
Philip Japikse <skimedic@gmail.com>
2024-10-04 15:22:36 -0400

    formatting

--- Code/CompletedLabs/Lab_Blazor09/AutoLot.ShortApi/Program.cs
+++ Code/CompletedLabs/Lab_Blazor09/AutoLot.ShortApi/Program.cs
@@ -13,7 +13,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options => options.AddDefaultPolicy(builder=>builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+builder.Services.AddCors(options => 
+    options.AddDefaultPolicy(builder=>
+        builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()));
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
